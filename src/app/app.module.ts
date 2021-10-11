748d86d58bdbe68448f1d4d545a41fda87745c26
Steininger Markus <marksteini16@gmail.com>
2021-10-11 18:19:38 +0200

    #2: Update navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { FlexModule } from "@angular/flex-layout";
+import { WorkoutPlansComponent } from './workout-plans/workout-plans.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    WorkoutPlansComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FlexModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
