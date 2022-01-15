4a1264a9d131949d6f15643feea0ed83f4bf97be
Steininger Markus <marksteini16@gmail.com>
2022-01-15 02:02:26 +0100

    #85: Add basic login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  hide = true;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  login() {
+    this.router.navigate(["workoutplans"])
   }
-
 }
