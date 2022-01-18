142b2faa2db38c8d4a28467998673b7eb8e0a7fe
Steininger Markus <marksteini16@gmail.com>
2022-01-18 21:09:19 +0100

    #58: Fix searchable list & add itemClick emitting

--- src/app/searchable-list/searchable-list.component.ts
+++ src/app/searchable-list/searchable-list.component.ts
@@ -1,24 +1,14 @@
-import {
-  AfterViewInit,
-  Component,
-  EventEmitter,
-  Input,
-  OnInit,
-  Output,
-  QueryList,
-  Renderer2,
-  ViewChildren
-} from '@angular/core';
-import { MatExpansionPanelHeader } from "@angular/material/expansion";
+import { Component, EventEmitter, Input, OnInit, Output, QueryList, ViewChildren } from '@angular/core';
+import { MatExpansionPanel } from "@angular/material/expansion";
 
 @Component({
   selector: 'app-searchable-list',
   templateUrl: './searchable-list.component.html',
   styleUrls: ['./searchable-list.component.scss']
 })
-export class SearchableListComponent<T> implements OnInit, AfterViewInit {
-  @ViewChildren(MatExpansionPanelHeader)
-  headers!: QueryList<MatExpansionPanelHeader>;
+export class SearchableListComponent<T> implements OnInit {
+  @ViewChildren(MatExpansionPanel)
+  panels!: QueryList<MatExpansionPanel>;
 
   @Input()
   data!: T[];
@@ -31,14 +21,10 @@ export class SearchableListComponent<T> implements OnInit, AfterViewInit {
   shownData: T[] = [];
   filterText = "";
 
-  constructor(private renderer: Renderer2) { }
+  constructor() { }
 
   ngOnInit(): void {
     this.filterAndSort();
-  }
-
-  ngAfterViewInit(): void {
-    this.headers.forEach(h => this.renderer.listen(h, "click", _ => console.log("asd")));
   }
 
   filterAndSort() {
@@ -46,5 +32,10 @@ export class SearchableListComponent<T> implements OnInit, AfterViewInit {
         ? this.data.concat()
         : this.data.filter(d => this.shownProperty(d).toLowerCase().includes(this.filterText.toLowerCase()))
     ).sort((d1, d2) => this.shownProperty(d1).localeCompare(this.shownProperty(d2)));
+  }
+
+  onItemOpen(item: T, index: number) {
+    this.panels.get(index)!.close();
+    this.itemClick.emit(item);
   }
 }
