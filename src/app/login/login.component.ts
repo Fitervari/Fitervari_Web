da737b026504d68d2b1fe8a7ade85e9eaeacd31a
Steininger Markus <marksteini16@gmail.com>
2022-01-30 01:06:18 +0100

    #29: Cleanup login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,7 +7,7 @@ import { Router } from "@angular/router";
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  hide = true;
+  hidePassword = true;
 
   constructor(private router: Router) { }
 
