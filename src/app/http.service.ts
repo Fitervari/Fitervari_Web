39a52f1c3cc14f0693940e558cacb55ab90713bc
Steininger Markus <marksteini16@gmail.com>
2022-01-30 01:05:04 +0100

    #88: Update http service

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -6,13 +6,24 @@ import { DeviceType } from "./model/deviceType";
   providedIn: 'root'
 })
 export class HttpService {
-  private url = "http://localhost:8080/fitervari";
-
-  //private url = "https://fitervari.free.beeceptor.com";
+  private baseurl = "https://student.cloud.htl-leonding.ac.at/m.rausch-schott/fitervari/api";
+  private deviceTypesUrl = `${this.baseurl}/deviceGroups`;
 
   constructor(private http: HttpClient) { }
 
-  public getDevices() {
-    return this.http.get<DeviceType[]>(`${this.url}/devices`);
+  getDevices() {
+    return this.http.get<DeviceType[]>(this.deviceTypesUrl);
+  }
+
+  createDevice(device: DeviceType) {
+    return this.http.post(this.deviceTypesUrl, device);
+  }
+
+  updateDevice(device: DeviceType) {
+    return this.http.put(this.deviceTypesUrl, device);
+  }
+
+  deleteDevice(id: number) {
+    return this.http.delete(`${this.deviceTypesUrl}/${id}`);
   }
 }
